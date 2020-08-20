68a27a0051095fa5fd46fbf1b053b89765aff5be
seojunpark <seojun0080@gmail.com>
2020-08-20 19:35:34 +0900

    Updates

---
+++ precache-manifest.7f8acb2242d288355c6d88d79dd8fcb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11ecb93f63926559652b737ad7bdffcd",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "053946f0e5a689cb1320",
+    "url": "/movie_app_2020/static/css/main.929f8c12.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "053946f0e5a689cb1320",
+    "url": "/movie_app_2020/static/js/main.7dbf4e2e.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
